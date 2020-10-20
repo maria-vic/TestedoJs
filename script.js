811b4342547aa33f5a7ee594879d4aefc3af2028
Maria Victória <70585570+maria-vic@users.noreply.github.com>
2020-10-20 09:03:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const ano = 2020;
+
+let nascimento = prompt("Digite o seu ano de nascimento");
+
+let nomeUsuario = prompt("Digite o seu nome:");
+
+let idade = (ano-nascimento);
+
+alert(`Olá, seu nome é ${nomeUsuario} e você tem ${idade} anos`);
